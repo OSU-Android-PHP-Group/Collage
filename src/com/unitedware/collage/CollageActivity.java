666faf57288a3d19aae9117799a48cccec4be3a0
Ryan McGowan <ryan@ryanmcg.com>
2012-04-04 14:54:37 -0400

    Initial commit.

---
+++ src/com/unitedware/collage/CollageActivity.java
@@ -0,0 +1,13 @@
+package com.unitedware.collage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CollageActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
