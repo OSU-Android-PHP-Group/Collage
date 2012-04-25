f41e0bf04691cf85a615d62bb24408ada2b23c34
Mark Tareshawty <tarebyte@gmail.com>
2012-04-24 21:33:32 -0400

    Started setting up the ground work to make a dynamic gridview of photos

---
+++ src/com/unitedware/collage/ImageAdapter.java
@@ -0,0 +1,52 @@
+package com.unitedware.collage;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+	private Context mContext;
+	//ArrayList<Bitmap> uploadGallery;
+	//int galleryItem;
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		ImageView imageView;
+
+		// Sets up the gallery
+		if (convertView == null) {
+			imageView = new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(8, 8, 8, 8);
+		} else {
+			imageView = (ImageView) convertView;
+		}
+		
+		// I need to get the images from when they choose and add them to an ArrayList<BitMap>
+		//imageView.setImageResource(galleryItem);
+		return null;
+	}
+}
