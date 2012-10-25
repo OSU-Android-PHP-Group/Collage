f7d1661ba1930a5a92562d1ae9fd71b1114647ab
Mark Tareshawty <tarebyte@gmail.com>
2012-10-25 13:06:45 -0400

    started gallery function

--- src/com/unitedware/collage/SelectCollagePhotos.java
+++ src/com/unitedware/collage/SelectCollagePhotos.java
@@ -1,6 +1,28 @@
 package com.unitedware.collage;
 
-public class SelectCollagePhotos {
-	
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
+public class SelectCollagePhotos extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.photogallery);
+
+		GridView photoGrid = (GridView) findViewById(R.id.photo_grid);
+		photoGrid.setAdapter(new ImageAdapter(this));
+
+		// photoGrid.setOnItemClickListener(new OnItemClickListener() {
+		// public void onItemClick(AdapterView<?> parent, View v,
+		// int position, long id) {
+		// Toast.makeText(SelectCollagePhotos.this, "" + position,
+		// Toast.LENGTH_SHORT).show();
+		// }
+		// });
+	}
 }
