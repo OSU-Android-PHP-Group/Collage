2de3da70f0670cf2fd95024526752ad3da5776f7
Mark Tareshawty <tarebyte@gmail.com>
2012-11-01 06:37:41 -0400

    created galleryview and for right now pictures are clickable with Toast object

--- src/com/unitedware/collage/SelectCollagePhotos.java
+++ src/com/unitedware/collage/SelectCollagePhotos.java
@@ -1,6 +1,28 @@
 package com.unitedware.collage;
 
-public class SelectCollagePhotos {
-	
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
+public class SelectCollagePhotos extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gallery);
+
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new ImageAdapter(this));
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				Toast.makeText(SelectCollagePhotos.this, "" + position,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 }
