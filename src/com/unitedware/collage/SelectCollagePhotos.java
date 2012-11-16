1dca3befc29e137b50ed0de3ade0390e52e841e4
Ryan McGowan <ryan@ryanmcg.com>
2012-11-16 11:26:54 -0500

    Create collage and add files to it when photos are selected.

--- src/com/unitedware/collage/SelectCollagePhotos.java
+++ src/com/unitedware/collage/SelectCollagePhotos.java
@@ -1,7 +1,10 @@
 package com.unitedware.collage;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -9,17 +12,22 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 public class SelectCollagePhotos extends Activity {
+	public static final String TAG = "SelectCollagePhotos";
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gallery);
 
-		GridView gridview = (GridView) findViewById(R.id.gridview);
+		final GridView gridview = (GridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new ImageAdapter(this));
+		final Collage collage = new Collage();
 
 		gridview.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View v,
 					int position, long id) {
+				File image = (File) gridview.getAdapter().getItem(position);
+				collage.addImage(image);
+				Log.i(TAG, "Added image to collage (\"" + image.getName() + "\")");
 				Toast.makeText(SelectCollagePhotos.this, "" + "File has been chosen",
 						Toast.LENGTH_SHORT).show();
 			}
